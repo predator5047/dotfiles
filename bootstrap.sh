3ea2f458193239eef2a2e435b0c0c85e5829575a
Buţiu Alexandru Octavian <predator5047@gmail.com>
2015-05-18 22:57:10 +0300

    Create .vim/backup directory in home
    Signed-off-by: Buţiu Alexandru Octavian <predator5047@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,9 @@
 
 files=".bashrc .gitconfig .hgrc .muttrc .vimrc"
 
+echo "Creating vim backup directory"
+mkdir -p $HOME/.vim/backup
+
 for file in $files; do
 	echo "Copying file $file"
 	ln -s $PWD/$file $HOME/$file
