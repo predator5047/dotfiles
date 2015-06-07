ade6d01e9f2c5fd3560d83e7da65f75a4b03272d
Buţiu Alexandru Octavian <predator5047@gmail.com>
2015-06-07 14:29:40 +0300

    The bootstrap script now links to any directory and handles conflicts files
    Previously the bootstrap script would only link dot files to the home
    directory not it can link them anywhere and can now handle conflicts.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,30 @@ echo "Creating vim backup directory"
 mkdir -p $HOME/.vim/backup
 
 for file in $files; do
-	echo "Copying file $file"
-	ln -s $PWD/$file $HOME/$file
+	dir=$(dirname "$file")
+	if [[ $dir = "." ]]; then
+		dir=$HOME
+	else
+		file=$(basename $file)
+		mkdir -p $dir	
+	fi
+
+	echo $dir/$file $file
+	if [[ -f "$dir/$file" ]]; then
+		dont_replace=false
+		while true; do
+			read -p "Do you want to replace the file $file? (y/n/d) " ynd
+			case $ynd in
+				[Yy]* ) rm -f "$dir/$file"; break;;
+				[Nn]* ) dont_replace=true; break;;
+				[Dd]* ) diff -u "$dir/$file" "$file";;
+				[Qq]* ) exit;;
+			esac
+		done
+	fi
+
+	if [[ ! "$dont_replace" ]]; then
+		echo "Copying file $file"
+		ln -s $PWD/$file $dir/$file
+	fi
 done
