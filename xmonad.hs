236b99c9f51b203d1743b513788e18037c948119
Buţiu Alexandru Octavian <predator5047@gmail.com>
2015-12-15 19:21:00 +0200

    Changed modMask to Super and volume increment to 5%

--- xmonad.hs
+++ xmonad.hs
@@ -15,14 +15,16 @@ main = do
 myConfig = defaultConfig {
 		workspaces = myworkspaces,
 		terminal = "urxvt",
+		modMask = mod4Mask,
 		manageHook = manageDocks <+> (isFullscreen --> doFullFloat) <+> manageHook defaultConfig <+> manageDocks,
 		layoutHook = smartBorders . layoutHook $ defaultConfig
 	} `additionalKeys` [
-		((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -- -3%"),
-		((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -- +3%"),
+		((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -- -5%"),
+		((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -- +5%"),
 		((0, xF86XK_AudioMute), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
 	]
 
 
 myworkspaces = ["1", "2", "3", "4", "5"]
-startupApps = ["google-chrome-stable", "xset s off"]
+startupApps = [ "google-chrome", "xset s off", "skype"
+	, "feh --bg-scale /home/alexandru/Pictures/1920x1080\\ HDTV.jpg" ]
