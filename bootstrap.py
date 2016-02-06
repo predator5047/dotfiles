ca3cba8a1e69aec7ec59dfb3d3cf1f7bb3e7e340
Buţiu Alexandru Octavian <predator5047@gmail.com>
2016-02-06 11:54:18 +0200

    Rewritten bootstrap in python

---
+++ bootstrap.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+
+files = {
+        ".bashrc" : "~",
+        ".gitconfig" : "~",
+        ".hgrc" : "~",
+        ".muttrc" : "~",
+        ".vimrc" : "~",
+        ".xmobarrc" : "~",
+        "xmonad.hs" : "~/.xmonad",
+        ".Xresources" : "~",
+}
+
+def link(dir, file, path):
+    print("Linking file {} to path {}".format(file, path))
+    os.makedirs(dir, exist_ok=True)
+
+    try:
+        os.remove(path)
+    except:
+        pass #file doesn't exist
+
+    current_dir, _ = os.path.split(os.path.realpath(__file__))
+    file = os.path.join(current_dir, file)
+    os.symlink(file, path)
+
+def main():
+    for f, p in files.items():
+        dir = os.path.expanduser(p)
+        path = os.path.join(dir, f)
+
+        if os.path.isfile(path):
+            res = input("Found file {} should I replace it? [y/n]? ".format(f))
+            if res.lower() == "y":
+                link(dir, f, path)
+        else:
+            link(dir, f, path)
+    
+if __name__ == "__main__":
+    main()
