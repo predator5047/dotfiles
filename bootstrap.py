7aec7e6e563f38fc5efc9bba4873ed10b96d293d
Buţiu Alexandru Octavian <predator5047@gmail.com>
2016-02-10 10:29:05 +0200

    Added cli's to link all files found or ignore them

--- bootstrap.py
+++ bootstrap.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import getopt as go
+import sys
 
 files = {
         ".bashrc" : "~",
@@ -13,6 +15,11 @@ files = {
         "getvolume.sh" : "~/.xmonad",
         ".Xresources" : "~",
 }
+
+class Answer:
+    YES = 0
+    NO = 1
+    NONE = 2
 
 def link(dir, file, path):
     print("Linking file {} to path {}".format(file, path))
@@ -27,17 +34,35 @@ def link(dir, file, path):
     file = os.path.join(current_dir, file)
     os.symlink(file, path)
 
-def main():
+def main(answer=Answer.NONE):
     for f, p in files.items():
         dir = os.path.expanduser(p)
         path = os.path.join(dir, f)
 
         if os.path.isfile(path):
-            res = input("Found file {} should I replace it? [y/n]? ".format(f))
-            if res.lower() == "y":
+            if answer == Answer.NO:
+                print("Found file {} ignoring".format(f))
+            elif answer == Answer.YES:
                 link(dir, f, path)
+            else:
+                res = input("Found file {} should I replace it? [y/n]? ".format(f))
+                if res.lower() == "y":
+                    link(dir, f, path)
         else:
             link(dir, f, path)
     
 if __name__ == "__main__":
-    main()
+    try:
+        opts, args = go.getopt(sys.argv[1:], "yn", ["yes", "no"])
+    except go.GetoptError:
+        print("wrong arguments")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ["-y", "--yes"]:
+            main(Answer.YES)
+        elif opt in ["-n", "--no"]:
+            main(Answer.NO)
+
+    if len(sys.argv) == 1:
+        main()
